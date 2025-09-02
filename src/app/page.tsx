704ca73002c5f6dbb1702a2e007c78a30d7e8cdb
youserstack <youserstack@gmail.com>
2025-09-02 12:01:27 +0900

    rocket ai assignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,29 @@
-import Image from "next/image";
+import Sajupalza from "@/components/sajupalza";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main>
+      <section>
+        <h1 className="sr-only">Rocket AI 과제</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        <div className="bg-zinc-100 h-[1000px] relative ">
+          {/* 타이틀 */}
+          <div className="text-center  translate-y-20">
+            <h1>제 1장</h1>
+            <p>나의 사주팔자</p>
+          </div>
+
+          {/* 말풍선 */}
+          <div
+            className="w-1/2 aspect-3/2 p-4 border flex items-center justify-center rounded-[50%/50%] text-center bg-white
+            absolute top-1/2 left-1/12 -translate-y-1/2"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            이제 본격적으로 로켓님의 사주팔자를 분석해볼 차례네요
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <Sajupalza />
+      </section>
+    </main>
   );
 }
---
+++ src/components/sajupalza.tsx
@@ -0,0 +1,115 @@
+export default function Sajupalza() {
+  // 테이블 컬럼
+  const columns = [
+    { han: "時", kor: "시" },
+    { han: "日", kor: "일" },
+    { han: "月", kor: "월" },
+    { han: "年", kor: "년" },
+  ];
+
+  // 테이블 행
+  const rows = [
+    {
+      title: { han: "十星", kor: "십성" },
+      data: [
+        { han: "傷官", kor: "상관" },
+        { han: "比肩", kor: "비견" },
+        { han: "傷官", kor: "상관" },
+        { han: "傷官", kor: "상관" },
+      ],
+    },
+    {
+      title: { han: "天干", kor: "천간" },
+      data: [
+        { han: "甲", kor: "갑" },
+        { han: "乙", kor: "을" },
+        { han: "丙", kor: "병" },
+        { han: "丁", kor: "정" },
+      ],
+    },
+    {
+      title: { han: "地支", kor: "지지" },
+      data: [
+        { han: "子", kor: "자" },
+        { han: "丑", kor: "축" },
+        { han: "寅", kor: "인" },
+        { han: "卯", kor: "묘" },
+      ],
+    },
+    {
+      title: { han: "十二運星", kor: "십이운성" },
+      data: [
+        { han: "長生", kor: "장생" },
+        { han: "沐浴", kor: "목욕" },
+        { han: "冠帶", kor: "관대" },
+        { han: "建祿", kor: "건록" },
+      ],
+    },
+    {
+      title: { han: "十二神煞", kor: "십이신살" },
+      data: [
+        { han: "天殺", kor: "천살" },
+        { han: "鬼殺", kor: "귀살" },
+        { han: "華殺", kor: "화살" },
+        { han: "災殺", kor: "재살" },
+      ],
+    },
+    {
+      title: { han: "貴人", kor: "귀인" },
+      data: [
+        { han: "太乙", kor: "태인" },
+        { han: "月德", kor: "월덕" },
+        { han: "天德", kor: "천덕" },
+        { han: "人德", kor: "인덕" },
+      ],
+    },
+  ];
+
+  return (
+    <div>
+      <div className="text-center py-10">
+        <h1 className="text-lg">김로켓님의 사주</h1>
+        <p className="text-2xl">1980년 8월 27일 08:10</p>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            {columns.map((col, i) => (
+              <th key={i}>
+                {col.han}
+                <br />({col.kor})
+              </th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {rows.map((row, i) => (
+            <tr key={i}>
+              <th>
+                {row.title.han}
+                <br />({row.title.kor})
+              </th>
+              {row.data.map((cell, j) => (
+                <td key={j}>
+                  <div
+                    className={
+                      row.title.han === "天干" || row.title.han === "地支"
+                        ? "bg-gray-100 rounded-lg" // 천간, 지지 행 데이터 배경색
+                        : ""
+                    }
+                  >
+                    {cell.han}
+                    <br />({cell.kor})
+                  </div>
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
