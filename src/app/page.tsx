0a0b0a24bfa8a03cbf89d9e51196589959d06c5a
youserstack <youserstack@gmail.com>
2025-09-05 10:10:17 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -122,7 +122,10 @@ export default function Home() {
               width={1000}
               height={1000}
             />
-            <div className="absolute left-0 right-0 bottom-0 h-1/2 bg-gradient-to-b from-transparent to-zinc-50"></div>
+            <div
+              className="absolute left-0 right-0 bottom-0 h-1/2 z-[5]
+              bg-gradient-to-b from-transparent to-zinc-50"
+            ></div>
           </div>
           <Sajupalza />
         </div>
